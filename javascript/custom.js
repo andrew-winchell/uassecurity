0a00631096c52447e9db7ebe65ed0aa7c88e048d
andrew-winchell <108092629+andrew-winchell@users.noreply.github.com>
2024-05-01 13:26:22 -0400

    Update custom.js

--- javascript/custom.js
+++ javascript/custom.js
@@ -1,64 +1,41 @@
 require([
     "esri/identity/OAuthInfo",
-    "esri/identity/IdentityManager",
-    "esri/portal/Portal",
-    "esri/portal/PortalQueryParams"
-], function (OAuthInfo, esriId, Portal, PortalQueryParams) {
+    "esri/identity/IdentityManager"
+], function (OAuthInfo, esriId) {
 
-    // window.onload = () => {
-    //     LoadSurvey123();
-    // }
-    
-    document.getElementById("sign-in").addEventListener('click', checkSignIn);
-
-    const info = new OAuthInfo({
-        appId: "0iQHRlu9bRvesIIV",
-        portalUrl: "https://faasysops.maps.arcgis.com",
-        authNamespace: "portal_oauth_inline",
-        flowtype: "auto",
-        popup: false
-    });
-
-    esriId.registerOAuthInfos([info]);
-
-    function checkSignIn() {
-        esriId
-            .checkSignInStatus(info.portalUrl + "/sharing")
-            .then(() => {
-                // If signed in, show the username in the UI
-                console.log("signed in")
-                const portal = new Portal({
-                    authMode: "immediate"
-                });
-                
-                if (info.portalUrl !== "https://www.arcgis.com") {
-                    portal.url = info.portalUrl;
-                }
-
-                portal.load().then(() => {
-                    console.log(portal.user.fullName);
-                    console.log(portal.user.username);
-                });
-            })
-            .catch(() => {
-                console.log("not signed in");
-                signIn();
-            })
+    window.onload = () => {
+        LoadSurvey123();
     }
+    
+    document.getElementById("sign-in").addEventListener('click', userAuth);
 
-    function signIn() {
-        esriId
-            .getCredential(info.portalUrl + "/sharing", {
-                oAuthPopupConfirmation: true
-            })
-            .then(() => {
-                LoadSurvey123();
-                console.log("signed in")
+    function userAuth() {
+        const info = new OAuthInfo({
+            appId: "0iQHRlu9bRvesIIV",
+            portalUrl: "https://faasysops.maps.arcgis.com",
+            authNamespace: "portal_oauth_inline",
+            flowtype: "auto",
+            popup: false
+        });
+
+        esriId.registerOAuthInfos([info]);
+
+        esriId.getCredential(info.portalUrl + "/sharing")
+            .then((credentials) => {
+                let token = credentials.token;
+                LoadSurvey123(token);
             });
+
+        // esriId.checkSignInStatus(info.portalUrl + "/sharing")
+        //     .then(() => {
+        //         console.log("Sign in Successfull.");
+        //     })
+        //     .catch(() => {
+        //         console.log("Public User");
+        //     });
     }
 
     function LoadSurvey123() {
-        console.log("load survey");
         let url_string = window.location.href;
         let url = new URL(url_string);
         let webform = new Survey123WebForm({
