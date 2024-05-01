071dfb693d92cd3b9b5df8a9ed613659068f362c
andrew-winchell <108092629+andrew-winchell@users.noreply.github.com>
2024-05-01 12:35:35 -0400

    Update custom.js

--- javascript/custom.js
+++ javascript/custom.js
@@ -1,39 +1,69 @@
 require([
     "esri/identity/OAuthInfo",
-    "esri/identity/IdentityManager"
-], function (OAuthInfo, esriId) {
+    "esri/identity/IdentityManager",
+    "esri/portal/Portal",
+    "esri/portal/PortalQueryParams"
+], function (OAuthInfo, esriId, Portal, PortalQueryParams) {
 
     // window.onload = () => {
     //     LoadSurvey123();
     // }
     
     document.getElementById("sign-in").addEventListener('click', userAuth);
-
+    
     function userAuth() {
+
         const info = new OAuthInfo({
             appId: "0iQHRlu9bRvesIIV",
             portalUrl: "https://faasysops.maps.arcgis.com",
             authNamespace: "portal_oauth_inline",
             flowtype: "auto",
-            popup: true
+            popup: false
         });
-
+    
         esriId.registerOAuthInfos([info]);
+    
+        checkSignIn();
 
-        esriId.getCredential(info.portalUrl + "/sharing")
-            .then((credentials) => {
-                let token = credentials.token;
-                console.log(token)
-                LoadSurvey123(token);
-            });
+        // esriId.getCredential(info.portalUrl + "/sharing")
+        //     .then((credentials) => {
+        //         let token = credentials.token;
+        //         console.log(token)
+        //         LoadSurvey123(token);
+        //     });
 
-        esriId.checkSignInStatus(info.portalUrl + "/sharing")
+        // esriId.checkSignInStatus(info.portalUrl + "/sharing")
+        //     .then(() => {
+        //         console.log("Sign in Successfull.");
+        //     })
+        //     .catch(() => {
+        //         console.log("Public User");
+        //     });
+    }
+
+    function checkSignIn() {
+        esriId
+            .checkSignInStatus(info.portalUrl + "/sharing")
             .then(() => {
-                console.log("Sign in Successfull.");
+                // If signed in, show the username in the UI
+                navigationUser.hiden = false;
+                signInButton.hidden = true;
+                const portal = new Portal({
+                    authMode: "immediate"
+                });
+                
+                if (info.portalUrl !== "https://www.arcgis.com") {
+                    portal.url = info.portalUrl;
+                }
+
+                portal.load().then(() => {
+                    navigationUser.fullName = portal.user.fullName;
+                    navigatorUser.username = portal.user.username;
+                })
             })
             .catch(() => {
-                console.log("Public User");
-            });
+                console.log("not signed in");
+            })
     }
 
     function LoadSurvey123(token) {
