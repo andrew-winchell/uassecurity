a7faf2e969321d086493b1cbc1b768c5e798ae11
andrew-winchell <108092629+andrew-winchell@users.noreply.github.com>
2024-05-01 13:46:22 -0400

    Update custom.js

--- javascript/custom.js
+++ javascript/custom.js
@@ -3,51 +3,39 @@ require([
     "esri/identity/IdentityManager"
 ], function (OAuthInfo, esriId) {
 
-    window.onload = () => {
-        LoadSurvey123();
+    // window.onload = () => {
+    //     LoadSurvey123();
+    // }
+    
+    document.getElementById("sign-in").addEventListener('click', userAuth);
+
+    function userAuth() {
+        const info = new OAuthInfo({
+            appId: "NlofBsaSFmnfbANf",
+            portalUrl: "https://cobecconsulting.maps.arcgis.com",
+            authNamespace: "portal_oauth_inline",
+            flowtype: "auto",
+            popup: false
+        });
+
+        esriId.registerOAuthInfos([info]);
+
+        esriId.getCredential(info.portalUrl + "/sharing")
+            .then((credentials) => {
+                let token = credentials.token;
+                LoadSurvey123(token);
+            });
+
+        // esriId.checkSignInStatus(info.portalUrl + "/sharing")
+        //     .then(() => {
+        //         console.log("Sign in Successfull.");
+        //     })
+        //     .catch(() => {
+        //         console.log("Public User");
+        //     });
     }
 
-    const info = new OAuthInfo({
-        appId: "NlofBsaSFmnfbANf",
-        portalUrl: "https://cobecconsulting.maps.arcgis.com",
-        authNamespace: "portal_oauth_inline",
-        flowtype: "auto",
-        popup: false
-    });
-
-    esriId.registerOAuthInfos([info]);
-
-    let credential = esriId.getCredential(info.portalUrl + "/sharing");
-
-    // document.getElementById("sign-in").addEventListener('click', userAuth);
-
-    // function userAuth() {
-    //     const info = new OAuthInfo({
-    //         appId: "0iQHRlu9bRvesIIV",
-    //         portalUrl: "https://.maps.arcgis.com",
-    //         authNamespace: "portal_oauth_inline",
-    //         flowtype: "auto",
-    //         popup: false
-    //     });
-
-    //     esriId.registerOAuthInfos([info]);
-
-    //     esriId.getCredential(info.portalUrl + "/sharing")
-    //         .then((credentials) => {
-    //             let token = credentials.token;
-    //             LoadSurvey123(token);
-    //         });
-
-    //     // esriId.checkSignInStatus(info.portalUrl + "/sharing")
-    //     //     .then(() => {
-    //     //         console.log("Sign in Successfull.");
-    //     //     })
-    //     //     .catch(() => {
-    //     //         console.log("Public User");
-    //     //     });
-    // }
-
-    function LoadSurvey123() {
+    function LoadSurvey123(token) {
         let url_string = window.location.href;
         let url = new URL(url_string);
         let webform = new Survey123WebForm({
@@ -55,7 +43,7 @@ require([
             itemId: '7b4bd8a23e784a068b6c05626c36dd6d',
             portalUrl: 'https://faasysops.maps.arcgis.com',
             width: '1200',
-            // token: token,
+            token: token,
             globalId: url.searchParams.get("globalid"),
             mode: url.searchParams.get("globalid")==null?'':'edit',
             onFormSubmitted: (data) => {window.parent.scrollTo(0,0)}
